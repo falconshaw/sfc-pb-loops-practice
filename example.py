ce5ed62a29a73191950c2a69409464846665ec93
Warren Longmire <warrenlongmire@gmail.com>
2024-06-04 17:37:35 -0400

    modified solution.

--- example.py
+++ example.py
@@ -11,8 +11,23 @@ for i in range(0, 4):
     for j in range(0, 4):
         print("X", end="")
 
+
 print("\n")
-print("Image 3: Triangle")
+print("Image 3: The While Box")
+continue_box = "y"
+while(continue_box == "y"):
+    rows = int(input("Enter the number of rows: "))
+    cols = int(input("Enter the number of cols: "))
+
+    for j in range(0, rows):
+        print()
+        for j in range(0, cols):
+            print("X", end="")
+
+    continue_box = input("\nDo you want to continue? (y/n): ")
+
+print("\n")
+print("Image 4: The Triangle")
 
 len = 0
 for i in range(0, 4):
@@ -22,7 +37,7 @@ for i in range(0, 4):
         print("X", end="")
 
 print("\n")
-print("Image 4: The Triangle Application")
+print("Image 5: The Triangle Application")
 continue_program = True
 while(continue_program):
     rows = int(input("Enter the number of rows: "))
@@ -35,7 +50,7 @@ while(continue_program):
         for j in range(0, len):
             print(character, end="")
     
-    if (input("\nDo you want to continue? (Y/N): ") == "N"):
+    if (input("\nDo you want to continue? (y/n): ") == "n"):
         continue_program = False
     else:
         continue_program = True
