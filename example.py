98e2f0d2229036688374aef6c75431471da02989
Warren Longmire <warrenlongmire@gmail.com>
2024-06-04 17:55:45 -0400

    adds starter code.

--- example.py
+++ example.py
@@ -1,58 +1,57 @@
 print("Pretty Printer Text Formatter:")
 
 print("Image 1: The Line")
-for i in range(1, 4):
-    print("X", end="")
+###Use a for loop to print a line of 4 "X" characters.
 
 print("\n")
 print("Image 2: The Box")
-for i in range(0, 4):
-    print()
-    for j in range(0, 4):
-        print("X", end="")
+###Complete the following pseudocode to create a box of 4 rows and 4 columns of
+###the character "$".
+
+###For each row of 4 rows
+    ###For each column of 4 columns
+        ###Print an "$"
+    ###Print a newline
 
 
 print("\n")
 print("Image 3: The While Box")
+###Complete the following code to create a box of an arbitrary size a user
+###specified character.
+
 continue_box = "y"
-while(continue_box == "y"):
+##Add a loop that will continue while continue_box is equal to "y"
     rows = int(input("Enter the number of rows: "))
     cols = int(input("Enter the number of cols: "))
+    character = input("Enter the character that should be printed: ")
 
-    for j in range(0, rows):
-        print()
-        for j in range(0, cols):
-            print("X", end="")
+    ##Add a nested loop that will print a box of the size specified by the user
+        ###Print the character specified by the user
 
     continue_box = input("\nDo you want to continue? (y/n): ")
 
 print("\n")
 print("Image 4: The Triangle")
+###Follow the following pseudocode to create a 4 row tall triangle composed of 
+###the character "X".
 
-len = 0
-for i in range(0, 4):
-    print()
-    len = len + 1
-    for j in range(0, len):
-        print("X", end="")
+###The first row should contain 1 "X", with each subsequent row containing one 
+###additional "X".
+
+#Set Len to 0
+##Enter row loop here
+    ##Print a space
+    ##Increment len
+    ##Use a loop to print len number of "X"s
 
 print("\n")
 print("Image 5: The Triangle Application")
-continue_program = True
-while(continue_program):
-    rows = int(input("Enter the number of rows: "))
-    character = input("Enter the input character: ")
+###Complete all of the following code to create a triangle of an arbitrary
+###size.
 
-    len = 0
-    for i in range(0, rows):
-        print()
-        len = len + 1
-        for j in range(0, len):
-            print(character, end="")
-    
-    if (input("\nDo you want to continue? (y/n): ") == "n"):
-        continue_program = False
-    else:
-        continue_program = True
+###The user should be able to continue to create triangles until 
+### continue_program is equal to "n".
+
+##Enter your code here
 
 print("Program Completed!")
